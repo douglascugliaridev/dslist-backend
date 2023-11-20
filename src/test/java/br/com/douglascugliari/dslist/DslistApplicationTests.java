60c91ca0d9699ee523993c219afd890459dabf1e
douglas.santos <douglas.santos@softplan.com.br>
2023-11-20 10:36:37 -0300

    Project created and properties, Game entity, ORM

---
+++ src/test/java/br/com/douglascugliari/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.douglascugliari.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
